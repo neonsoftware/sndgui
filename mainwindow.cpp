e360a43ce81387b5d4efcf361e1d0422ba083b02
sammythegoat <sammythegoat@sammythegoats-MacBook-Pro.local>
2017-10-19 21:52:14 +0200

    Added plus and minus

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "QtCore"
+#include "QtWidgets"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +13,22 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_actionAdd_triggered()
+{
+     ui->listWidget->addItems(QFileDialog::getOpenFileNames());
+}
+
+void MainWindow::on_actionRemove_triggered()
+{
+    for(auto item : ui->listWidget->selectedItems()){
+        ui->listWidget->takeItem(ui->listWidget->row(item));
+
+    }
+}
+
+void MainWindow::on_actionRun_triggered()
+{
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -15,6 +15,13 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_actionAdd_triggered();
+
+    void on_actionRemove_triggered();
+
+    void on_actionRun_triggered();
+
 private:
     Ui::MainWindow *ui;
 };
