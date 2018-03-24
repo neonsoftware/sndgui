ae96df679da6188844fcdd78fa5fb0d422ad449e
neon <neon@local>
2018-03-24 23:42:28 +0100

    Using Darkstyle

--- src/main_editor.cpp
+++ src/main_editor.cpp
@@ -5,6 +5,11 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
+    QFile f(":/qdarkstyle/style.qss");
+    f.open(QFile::ReadOnly | QFile::Text);
+    QTextStream ts(&f);
+    a.setStyleSheet(ts.readAll());
+    
     EditDialog e;
     e.show();
     
